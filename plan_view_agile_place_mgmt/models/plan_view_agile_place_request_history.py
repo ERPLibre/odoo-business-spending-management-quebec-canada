90acdd2a24e59ff784e186a6d29394b51232ba47
Mathieu Benoit <mathben@technolibre.ca>
2024-10-24 07:01:48 -0400

    WIP show requests

--- plan_view_agile_place_mgmt/models/plan_view_agile_place_request_history.py
+++ plan_view_agile_place_mgmt/models/plan_view_agile_place_request_history.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# © 2021-2024 TechnoLibre (http://www.technolibre.ca)
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl)
+
 from odoo import _, api, fields, models
 
 
@@ -5,9 +9,35 @@ class PlanViewAgilePlaceRequestHistory(models.Model):
     _name = "plan.view.agile.place.request_history"
     _description = "plan_view_agile_place_request_history"
 
-    name = fields.Char()
+    name = fields.Char(string="URL", readonly=True, required=True)
+
+    send_data = fields.Char(readonly=True)
+
+    response_data = fields.Char(readonly=True)
 
     session_id = fields.Many2one(
         comodel_name="plan.view.agile.place.session",
         string="Session",
+        readonly=True,
     )
+
+    is_success = fields.Boolean(
+        string="Is success",
+        help="Is success when got response status_code 200",
+        readonly=True,
+    )
+
+    status_code = fields.Integer(readonly=True)
+
+    type = fields.Selection(
+        selection=[
+            ("get", "GET"),
+            ("post", "POST"),
+            ("delete", "DELETE"),
+        ],
+        required=True,
+        default="get",
+        readonly=True,
+    )
+
+    request_server_date = fields.Datetime(readonly=True)
--- plan_view_agile_place_mgmt/models/plan_view_agile_place_session.py
+++ plan_view_agile_place_mgmt/models/plan_view_agile_place_session.py
@@ -29,23 +29,49 @@ class PlanViewAgilePlaceSession(models.Model):
         string="Boards",
     )
 
+    request_history_ids = fields.One2many(
+        comodel_name="plan.view.agile.place.request_history",
+        inverse_name="session_id",
+        string="Requests",
+    )
+
     def request_api_get(self, path, data=None):
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + self.api_token,
         }
         url = self.name + path
+        json_data = None
         if data:
-            response = requests.get(
-                url, headers=headers, data=json.dumps(data)
-            )
+            json_data = json.dumps(data)
+            response = requests.get(url, headers=headers, data=json_data)
         else:
             response = requests.get(url, headers=headers)
+
+        response_data = json.loads(response.text)
+
+        request_history_value = {
+            "name": url,
+            "type": "get",
+            "session_id": self.id,
+            "is_success": response.status_code == requests.codes.ok,
+            "status_code": response.status_code,
+            "request_server_date": response.headers.get("Date"),
+            "response_data": response_data,
+        }
+
+        if data:
+            request_history_value["send_data"] = json_data
+
+        request_history_id = self.env[
+            "plan.view.agile.place.request_history"
+        ].create(request_history_value)
+
         # print(response)
         # print(response.status_code)  # Affiche le code de statut (ex: 200, 404)
         # print(response.text)  # Affiche le contenu de la réponse
         # print(response.headers)  # Affiche les en-têtes de la réponse
-        return response.status_code, json.loads(response.text)
+        return response.status_code, response_data
 
     def request_api_post(self, path, data):
         pass
